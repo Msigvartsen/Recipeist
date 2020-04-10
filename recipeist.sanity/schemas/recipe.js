908b2ea507b9807a2c47d8b736955d0d83799538
Magnus Sigvartsen <magnus.sigvartsen@novacare.no>
2020-04-10 13:29:20 +0200

    Add image and text propert to recipe document

--- recipeist.sanity/schemas/recipe.js
+++ recipeist.sanity/schemas/recipe.js
@@ -4,6 +4,8 @@ export default {
     type: 'document',
     fields: [
         { name: 'title', title: 'Title', type: 'string' },
+        { name: 'image', title: 'Image', type: 'image' },
+        { name: 'mainintro', title: 'Main Intro', type: 'text' },
         { name: 'ingredients', title: 'Ingredients', type: 'array', of: [{ type: 'ingredients' }] },
         { name: 'mainbody', title: 'Main Body', type: 'array', of: [{ type: 'block' }] }
     ]
