2c686b97c621e9f9fe4a696b5b5bb9d1b8ccf1d9
Dagfinn Reitan <dagfinn.reitan@novacare.no>
2020-05-07 23:59:40 +0200

    Linting config updates 🤘

--- recipeist.functions/sanity.js
+++ recipeist.functions/sanity.js
@@ -1,8 +1,8 @@
-const sanityClient = require('@sanity/client');
+import sanityClient from '@sanity/client';
 
 export const client = sanityClient({
-    projectId: 'zykf8mvo',
-    dataset: 'production',
-    token: 'skel8lxSxJIYASDBaqGwCcypQ6FBTAURy3lYAzxEYBazAZcZCK0seMexlujYt57OnkWF3v77DeJ6aS69tkr5kSXqRCBTn32mdHmhWbWST8aCZLVJKNbadAZ2URzxg7I4e7TMgdXJca79TYfWXBuKVVNcyeha3NUeD6zYTw042Llu5zgJ1vbh', // or leave blank to be anonymous user
-    useCdn: true // `false` if you want to ensure fresh data
+  projectId: 'zykf8mvo',
+  dataset: 'production',
+  token: 'skel8lxSxJIYASDBaqGwCcypQ6FBTAURy3lYAzxEYBazAZcZCK0seMexlujYt57OnkWF3v77DeJ6aS69tkr5kSXqRCBTn32mdHmhWbWST8aCZLVJKNbadAZ2URzxg7I4e7TMgdXJca79TYfWXBuKVVNcyeha3NUeD6zYTw042Llu5zgJ1vbh', // or leave blank to be anonymous user
+  useCdn: true // `false` if you want to ensure fresh data
 });
