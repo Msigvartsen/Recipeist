79445c6d0dd63d9f6ddece26de7fd85b07683246
Magnus Sigvartsen <magnus.sigvartsen@gmail.com>
2020-05-10 20:01:57 +0200

    Added block-content render component

--- recipeist.vue/src/main.js
+++ recipeist.vue/src/main.js
@@ -1,6 +1,9 @@
 import Vue from "vue"
 import App from "./App.vue"
 import { router } from "./router"
+import BlockContent from "sanity-blocks-vue-component"
+
+Vue.component("block-content", BlockContent)
 
 Vue.config.productionTip = false
 
