d7c63007ebe3620b8f46de68a5c7981e4b93d108
Dagfinn Reitan <dagfinn.reitan@novacare.no>
2020-05-14 20:31:24 +0200

    translate function error fix

--- recipeist.functions/translate.js
+++ recipeist.functions/translate.js
@@ -1,28 +1,31 @@
-const { Translate } = require('@google-cloud/translate').v2;
-const googleConfigJsonPath = "./google.json";
-require(googleConfigJsonPath)
+const { Translate } = require("@google-cloud/translate").v2
+const isDev = () => process.env.NODE_ENV === "development"
+process.env.GOOGLE_APPLICATION_CREDENTIALS = isDev() ? "../google.json" : "./google.json" // user-appdata-local-temp-mappa i Windows
+require(process.env.GOOGLE_APPLICATION_CREDENTIALS)
 
-const isDev = () => process.env.NODE_ENV === "development";
-process.env.GOOGLE_APPLICATION_CREDENTIALS = isDev() ? `.${googleConfigJsonPath}` : googleConfigJsonPath
-
-const translateClient = new Translate();
+const translateClient = new Translate()
 
 export const translateText = async (text, targetLang = null, sourceLang = null) => {
-    console.log(`Translating [${text}] from [${sourceLang ? sourceLang : "?"}] to [${targetLang}]`);
+  console.log(`Translating [${text}] from [${sourceLang ? sourceLang : "?"}] to [${targetLang}]`)
 
-    if (!text) {
-        const error = [`Invalid text '${text}'`];
-        console.log(error);
-        return [error];
-    }
+  if (!text) {
+    const error = [`Invalid text '${text}'`]
+    console.log(error)
+    return text
+  }
 
-    let options = { to: targetLang || "en" };
-    if (sourceLang) options.from = sourceLang;
+  let options = { to: targetLang || "en" }
+  if (sourceLang) options.from = sourceLang
 
-    const translations = await translateClient.translate(text, options);
-    const translation = translations[0];
+  try {
+    const translations = await translateClient.translate(text, options)
+    const translation = translations[0]
 
-    console.log(`Translation finished [${translation}]`);
+    console.log(`Translation finished [${translation}]`)
 
-    return translation;
+    return translation
+  } catch (error) {
+    console.log(`Translation failed [${error}]`)
+    return text
+  }
 }
